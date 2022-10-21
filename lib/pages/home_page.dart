e4ae208104874d41658b240fd033500d8b95bed3
KellyAscanio <kjascanio48@misena.edu.co>
2022-10-21 18:50:46 -0500

    se sube proyecto inicio de sesion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,11 +27,12 @@ class _HomePageState extends State<HomePage> {
     }));
   }
 //initState nos sirve para mantener actualizado
-  @override
-  void initState() {
+//AAAAAA
+  /*@override
+  void initState(){
     getDocId();
     super.initState();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
